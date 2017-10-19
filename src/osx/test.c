f9f1250073046ec364234b448572b6092428273a
krzydyn <krzydyn@gmail.com>
2017-10-20 00:08:48 +0200

    trying osx carbon

---
+++ src/osx/test.c
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <Carbon/Carbon.h>
+#include <HIToolbox.h>
+
+using namespace std;
+/*
+int main()
+{
+	    //CGEventFlags flags = CGEventSourceFlagsState(1);
+
+	while(true)
+	{
+		CGEventSourceStateID eventSource = kCGEventSourceStateCombinedSessionState;
+		bool b = CGEventSourceKeyState(eventSource, kVK_Command) && CGEventSourceKeyState(eventSource, kVK_ANSI_Period);
+		if(b) {
+			cout << "Yes" << endl;
+		}
+		else
+		{
+			cout << "No" << endl;
+		}
+	}
+
+	cout << "Hello world!" << endl;
+	return 0;
+}
+*/
+
+int main(int ArgC, char **ArgV)
+{
+    Rect rPos;
+    WindowRef Handle;
+    EventTypeSpec etsEventTypes[2];
+     
+    rPos.top = 200;
+    rPos.left = 300;
+    rPos.bottom = 400;
+    rPos.right = 500;
+ 
+    //Create the window
+    CreateNewWindow(kDocumentWindowClass, kWindowStandardDocumentAttributes | kWindowStandardHandlerAttribute, &rPos, &Handle);
+     
+    //Show the window
+    ShowWindow(Handle);
+     
+    RunApplicationEventLoop();
+    //Remove the winow
+    DisposeWindow(Handle);
+     
+    return 0;
+}
